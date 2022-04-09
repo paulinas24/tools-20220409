845b14c4e755ed70474a1638e25e2fed909dfc62
paulinas24 <ps56015@gmail.com>
2022-04-09 11:55:17 +0200

    css zmiany

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,22 @@ const path = require('path');
 const HTMLWebpaclPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ['./src/main.js', './src/style.css'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+      test: /.css$/, 
+      use: ['style-loader', 'css-loader'],
+    },
+    ],
   },
   plugins: [
     new HTMLWebpaclPlugin ({
       template: './src/index.html',
     }), 
-  ]
+  ],
 };
