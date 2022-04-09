6e9ffdaf7b3700da447bfc3db9397dc1b51c5019
paulinas24 <ps56015@gmail.com>
2022-04-09 10:55:45 +0200

    html webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpaclPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -6,4 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  plugins: [
+    new HTMLWebpaclPlugin ({
+      template: './src/index.html',
+    }), 
+  ]
 };
